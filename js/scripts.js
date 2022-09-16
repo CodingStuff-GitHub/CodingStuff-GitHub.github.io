b3a45d179d540bd4b3dc67f6e5c82d8e0536457a
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2022-09-17 03:57:02 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,14 @@
 window.addEventListener("DOMContentLoaded", (event) => {
+  // Activate Bootstrap scrollspy on the main nav element
+  try {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      offset: 74,
+    });
+  } catch (e) {
+    console.log(e);
+  }
+
   // Collapse responsive navbar when toggler is visible
   const navbarToggler = document.body.querySelector(".navbar-toggler");
   const responsiveNavItems = [].slice.call(

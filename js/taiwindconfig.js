6c9c803f516668db02a7b3d4dc31c14e1749fb01
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-12-03 14:50:17 +0530

    New Page

---
+++ js/taiwindconfig.js
@@ -0,0 +1,9 @@
+tailwind.config = {
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      content: ['"Architects Daughter"'],
+      header: ['"Architects Daughter"', '"Inter var", sans-serif'],
+    },
+  },
+};

4eea7bd59614706275e6cacfc001ccd974adc4ef
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-12-11 01:42:35 +0530

    Update taiwindconfig.js

--- js/taiwindconfig.js
+++ js/taiwindconfig.js
@@ -5,5 +5,20 @@ tailwind.config = {
       content: ['"Kalam"', '"Inter var", sans-serif'],
       header: ['"Architects Daughter"', '"Inter var", sans-serif'],
     },
+    extend: {
+      animation: {
+        "infinite-scroll": "infiniteScroll 60s linear infinite",
+      },
+      keyframes: {
+        infiniteScroll: {
+          "0%": {
+            transform: "translateX(0%)",
+          },
+          "100%": {
+            transform: "translateX(-100%)",
+          },
+        },
+      },
+    },
   },
 };

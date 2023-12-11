2cbe258d8bd4593aad42ec5d090889b7eb96ceb1
Kamal Singh <37046527+CodingStuff-GitHub@users.noreply.github.com>
2023-12-11 13:24:14 +0530

    Update functions.js

--- js/functions.js
+++ js/functions.js
@@ -1,33 +1,6 @@
 window.addEventListener("DOMContentLoaded", (event) => {
   darkmode();
-  navigationToggler();
 });
-
-/**
- * This function handles the toggling of a responsive navigation bar.
- * It collapses the navbar when the toggler is visible and toggles the menu icon when the toggle button is clicked.
- */
-function navigationToggler() {
-  const drawerButton = document.getElementById("drawer-button");
-  const sidebar = document.getElementById("logo-sidebar");
-  const menuIcon = document.getElementById("menuIcon");
-  function toggleSidebar() {
-    if (sidebar.classList.contains("translate-x-0")) {
-      sidebar.classList.remove("translate-x-0");
-      sidebar.classList.add("-translate-x-full");
-      menuIcon.setAttribute("icon", "mdi:menu");
-    } else {
-      sidebar.classList.add("translate-x-0");
-      sidebar.classList.remove("-translate-x-full");
-      menuIcon.setAttribute("icon", "mdi:close");
-    }
-  }
-
-  // Toggle sidebar visibility when drawer button is clicked
-  drawerButton.addEventListener("click", function () {
-    toggleSidebar();
-  });
-}
 
 function darkmode() {
   const themeToggleDarkIcon = document.getElementById("theme-toggle-dark-icon");
